1931bb6b81331119ada2f40773c82f6ce338fa68
Andriy Kryvyi <kryvyi.andriy@gmail.com>
2025-03-26 17:58:53 +0200

    Add solution for "Move Node" problem

---
+++ move_node/problem.py
@@ -0,0 +1,18 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+
+def move_node(source, dest):
+    # Your code goes here.
+    # Remember to return the context.
+    source, tmp = source.next, source
+    tmp.next = dest
+    return Context(source, tmp)
